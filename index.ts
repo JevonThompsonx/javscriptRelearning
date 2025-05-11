1634a5d2c56f5a987120bfefd8aee7a6388ecaf2
Jevon Thompson <JThompsonx00@gmail.com>
2025-05-11 13:41:17 -0700

    Started day1

---
+++ index.ts
@@ -0,0 +1,13 @@
+console.log("Hello from javascript")
+
+console.log("create variables:")
+
+let string1 = "Hello I am a string"
+const string2 = "Hello I am a immutable string"
+
+console.log(string1)
+console.log(string2)
+
+var string3 = "var can also be used"
+
+console.log(string3)
